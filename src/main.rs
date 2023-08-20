89cc3d2837ffab73199c8f3486f6306472e113a1
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-08-20 13:49:54 +0530

    wifi list added

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,51 @@
-use std::error::Error;
-
 use env_logger::Env;
-use log::info;
-use wifi_ctrl::sta;
 
+use log::{error, info};
 
+use wifi_ctrl::{sta, Result};
 
-async fn get_wifi_networks() -> Result<Vec<wifi_ctrl::sta::ScanResult>, Box<dyn Error>> {
-    // Initialize logging and other setup
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    info!("Starting get_wifi_networks example");
+async fn scan_wifi() -> Result {
+    let mut setup = sta::WifiSetup::new()?;
 
     // Set up Wi-Fi interface with fixed socket path
-    let mut setup = sta::WifiSetup::new()?;
     let proposed_path = "/var/run/wpa_supplicant/wlan0";
-
     setup.set_socket_path(proposed_path);
 
     let requester = setup.get_request_client();
-    let networks = requester.get_scan().await?;
+    let runtime = setup.complete();
 
-    // Shutdown and return the networks
-    requester.shutdown().await?;
-    
-    Ok(networks.to_vec())
+    let (_runtime, _app) = tokio::join!(
+        async move {
+            if let Err(e) = runtime.run().await {
+                error!("Error: {e}");
+            }
+        },
+        app(requester),
+    );
+    Ok(())
 }
+
+async fn app(requester: sta::RequestClient) -> Result {
+    info!("Requesting scan");
+    let scan = requester.get_scan().await?;
+    info!("Scan complete");
+    for scan in scan.iter() {
+        info!("   {:?}", scan);
+    }
+
+    requester.shutdown().await?;
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
-    match get_wifi_networks().await {
-        Ok(networks) => {
-            println!("Available Wi-Fi Networks:");
-            for network in networks {
-                println!("{:?}", network);
-            }
-        }
-        Err(err) => eprintln!("Error: {}", err),
+    // Initialize logging and other setup
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    info!("Starting wifi-sta example");
+
+    // Call the scan_wifi function to get the list of networks
+    match scan_wifi().await {
+        Ok(_) => info!("Scan complete"),
+        Err(e) => error!("Error: {}", e),
     }
 }
